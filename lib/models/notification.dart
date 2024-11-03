8d86f36232d49bd0d89b8c220093188e2bc74be6
TomerNoy <mail2tomer@gmail.com>
2024-11-03 10:10:40 +0200

    added notifications model

---
+++ lib/models/notification.dart
@@ -0,0 +1,29 @@
+import 'package:mongo_dart/mongo_dart.dart';
+
+class NotificationModel {
+  final bool isPushOn;
+  final String scheduledTaskId;
+  final DateTime lastModified;
+
+  NotificationModel({
+    required this.isPushOn,
+    required this.scheduledTaskId,
+    required this.lastModified,
+  });
+
+  factory NotificationModel.fromMap(Map<String, dynamic> map) {
+    return NotificationModel(
+      isPushOn: map['isPushOn'] as bool,
+      scheduledTaskId: map['scheduledTaskId'] as String,
+      lastModified: DateTime.parse(map['lastModified']),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'isPushOn': isPushOn,
+      'scheduledTaskId': scheduledTaskId,
+      'lastModified': lastModified.toIso8601String(),
+    };
+  }
+}
